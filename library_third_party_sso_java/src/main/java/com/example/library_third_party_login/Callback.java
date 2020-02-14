4b13c1f32f82299b389314feeaf1aa5bbe834d9a
Alan <alan.lai@cloud-interactive.com>
2020-02-14 15:52:00 +0800

    add project

---
+++ library_third_party_sso_java/src/main/java/com/example/library_third_party_login/Callback.java
@@ -0,0 +1,13 @@
+package com.example.library_third_party_login;
+
+
+import com.example.library_third_party_login.facebook.FacebookData;
+
+/**
+ * 　Created by Alan on 2017/8/7.
+ */
+
+public interface Callback {
+
+    void getFacebookData(FacebookData facebookData);
+}
--- third_party_sso_java/src/main/java/com/example/thirdpartysso/MainActivity.java
+++ third_party_sso_java/src/main/java/com/example/thirdpartysso/MainActivity.java
@@ -2,13 +2,28 @@ package com.example.thirdpartysso;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+
+import com.example.library_third_party_login.facebook.FacebookController;
 
 public class MainActivity extends AppCompatActivity {
+
+    private FacebookController mFacebookController;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        mFacebookController = FacebookController.newInstance(this);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        mFacebookController.onActivityResult(requestCode, resultCode, data);
     }
 }
