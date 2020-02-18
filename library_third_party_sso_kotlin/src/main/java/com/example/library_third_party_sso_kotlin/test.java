96fcd549f6fd387c3f819c11c364f99531177db0
Alan <alan.lai@cloud-interactive.com>
2020-02-18 14:04:11 +0800

    update google login

---
+++ library_third_party_sso_kotlin/src/main/java/com/example/library_third_party_sso_kotlin/test.java
@@ -0,0 +1,15 @@
+package com.example.library_third_party_sso_kotlin;
+
+/****************************************************
+ * Copyright (C) Alan Corporation. All rights reserved.
+ *
+ * Author: AlanLai
+ * Create Date: 2020-02-18
+ * Usage:
+ *
+ * Revision History
+ * Date         Author           Description
+ ****************************************************/
+
+public class test {
+}
